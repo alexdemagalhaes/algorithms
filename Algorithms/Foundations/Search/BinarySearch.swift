34eaeec2ef4c4bb28db0bcb78b4381aea6c4c829
Alex Machado <alexdemagalhaes@gmail.com>
2020-09-24 00:44:32 -0700

    Binary search

---
+++ Algorithms/Foundations/Search/BinarySearch.swift
@@ -0,0 +1,40 @@
+//
+//  BinarySearch.swift
+//  Algorithms
+//
+//  Created by Alex Magalhaes on 09/23/20.
+//  Copyright © 2020 Alex Machado. All rights reserved.
+//
+
+public class BinarySearch<ComparableType: Comparable>: SearchAlgorithm {
+    private let array: [ComparableType]
+
+    init(input: [ComparableType]) {
+        array = input
+    }
+
+    public func search(for value: ComparableType) -> Int? {
+        return search(for: value, p: 0, r: array.count)
+    }
+
+    public func search(for value: ComparableType, p: Int, r: Int) -> Int? {
+        guard p < r - 1 else {
+            if p >= 0 && p < array.count && array[p] == value {
+                return p
+            }
+            return nil
+        }
+
+        let q = (p + r) / 2
+        switch value {
+        case let v where v == array[q]:
+            return q
+        case let v where v < array[q]:
+            return search(for: value, p: p, r: q)
+        case let v where v > array[q]:
+            return search(for: value, p: q + 1, r: r)
+        default:
+            return nil
+        }
+    }
+}
--- AlgorithmsTests/Foundations/SearchTests.swift
+++ AlgorithmsTests/Foundations/SearchTests.swift
@@ -10,68 +10,69 @@
 import XCTest
 
 class SearchTests: XCTestCase {
-    private func testSearchAlgorithms<ComparableType: Comparable>(withInput input: [ComparableType], value: ComparableType, expectedIndex: Int?) {
-        let algorithms: [AnySearchAlgorithm<ComparableType>] = [
-            AnySearchAlgorithm(LinearSearch(input: input))
+    private func testSearchAlgorithms<ComparableType: Comparable>(withInput input: [ComparableType], value: ComparableType) {
+        let algorithms: [(AnySearchAlgorithm<ComparableType>, Int?)] = [
+            (AnySearchAlgorithm(LinearSearch(input: input)), input.firstIndex(of: value)),
+            (AnySearchAlgorithm(BinarySearch(input: input.sorted())), input.sorted().firstIndex(of: value))
         ]
-        algorithms.forEach { algorithm in
+        algorithms.forEach { (algorithm, expectedIndex) in
             XCTAssertEqual(algorithm.search(for: value), expectedIndex)
         }
     }
 
     func testEmptyIntArrayDoesNotContainValue() {
-        testSearchAlgorithms(withInput: [Int](), value: 42, expectedIndex: nil)
+        testSearchAlgorithms(withInput: [Int](), value: 42)
     }
 
     func testEmptyDoubleArrayDoesNotContainValue() {
-        testSearchAlgorithms(withInput: [Double](), value: 3.1415, expectedIndex: nil)
+        testSearchAlgorithms(withInput: [Double](), value: 3.1415)
     }
 
     func testSingleIntElementContainsValueCorrectly() {
-        testSearchAlgorithms(withInput: [23], value: 23, expectedIndex: 0)
+        testSearchAlgorithms(withInput: [23], value: 23)
     }
 
     func testSingleIntElementDoesNotContainValueCorrectly() {
-        testSearchAlgorithms(withInput: [23], value: 108, expectedIndex: nil)
+        testSearchAlgorithms(withInput: [23], value: 108)
     }
 
     func testSingleDoubleElementContainsValueCorrectly() {
-        testSearchAlgorithms(withInput: [1.99], value: 1.99, expectedIndex: 0)
+        testSearchAlgorithms(withInput: [1.99], value: 1.99)
     }
 
     func testSingleDoubleElementDoesNotContainValueCorrectly() {
-        testSearchAlgorithms(withInput: [1.99], value: 10.8, expectedIndex: nil)
+        testSearchAlgorithms(withInput: [1.99], value: 10.8)
     }
 
     func testIntArrayContainsValue1Correctly() {
-        testSearchAlgorithms(withInput: [5, 2, 4, 6, 1, 3], value: 6, expectedIndex: 3)
+        testSearchAlgorithms(withInput: [5, 2, 4, 6, 1, 3], value: 6)
     }
 
     func testIntArrayContainsValue2Correctly() {
-        testSearchAlgorithms(withInput: [5, 2, 4, 6, 1, 3], value: 5, expectedIndex: 0)
+        testSearchAlgorithms(withInput: [5, 2, 4, 6, 1, 3], value: 5)
     }
 
     func testIntArrayDoesNotContainValueCorrectly() {
-        testSearchAlgorithms(withInput: [5, 2, 4, 6, 1, 3], value: 7, expectedIndex: nil)
+        testSearchAlgorithms(withInput: [5, 2, 4, 6, 1, 3], value: 7)
     }
 
     func testDoubleArrayContainsValue1Correctly() {
-        testSearchAlgorithms(withInput: [0.5, 0.1, 34.5, 500, -45.0, -5.0], value: 0.1, expectedIndex: 1)
+        testSearchAlgorithms(withInput: [0.5, 0.1, 34.5, 500, -45.0, -5.0], value: 0.1)
     }
 
     func testDoubleArrayContainsValue2Correctly() {
-        testSearchAlgorithms(withInput: [0.5, 0.1, 34.5, 500, -45.0, -5.0], value: -5.0, expectedIndex: 5)
+        testSearchAlgorithms(withInput: [0.5, 0.1, 34.5, 500, -45.0, -5.0], value: -5.0)
     }
 
     func testDoubleArrayDoesNotContainValueCorrectly() {
-        testSearchAlgorithms(withInput: [0.5, 0.1, 34.5, 500, -45.0, -5.0], value: 1.1, expectedIndex: nil)
+        testSearchAlgorithms(withInput: [0.5, 0.1, 34.5, 500, -45.0, -5.0], value: 1.1)
     }
 
     func testSearchingThroughAThousandNumbersFindsValueCorrectly() {
-        testSearchAlgorithms(withInput: Numbers.thousandRandomNumbers, value: 163144, expectedIndex: 108)
+        testSearchAlgorithms(withInput: Numbers.thousandRandomNumbers, value: 163144)
     }
 
     func testSearchingThroughAThousandNumbersDoesNotFindValueCorrectly() {
-        testSearchAlgorithms(withInput: Numbers.thousandRandomNumbers, value: 481516, expectedIndex: nil)
+        testSearchAlgorithms(withInput: Numbers.thousandRandomNumbers, value: 481516)
     }
 }
